82fcd3c6e4b3655602f2b7b724ecfc36450ebe86
Hybusa <alkouzne007@gmail.com>
2023-08-16 16:24:35 +0300

    some minor adjustments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,8 @@
 package org.example;
 
 
-import org.example.enums.GeneratorType;
 import org.example.enums.MazePaths;
-import org.example.enums.RandomType;
 import org.example.model.ActualSolver;
-import org.example.objects.GeneratorRequest;
 
 import java.io.IOException;
 
@@ -13,21 +10,21 @@ public class App {
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
         System.out.println("Maze solved in : " + ActualSolver.SolveMaze(
-                MazePaths.MAZE_8.toString(),
+                MazePaths.MAZE_TEST2.toString(),
                 true,
                 true,
                 MazePaths.SAVE_PATH.toString(),
-                new GeneratorRequest(
-                        200,
-                        200,
-                        GeneratorType.DEPTH_FIRST_SEARCH,
-                        RandomType.TRUE_RANDOM,
-                        true))
-                );
+                null
+                ));
         long stop = System.currentTimeMillis();
         System.out.println("All process : " + (stop - start));
 
-
+//        new GeneratorRequest(
+//                200,
+//                200,
+//                GeneratorType.DEPTH_FIRST_SEARCH,
+//                RandomType.TRUE_RANDOM,
+//                true)
     }
 }
 
